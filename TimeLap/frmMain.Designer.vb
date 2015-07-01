8c090e1a1035f7e46489cebd700285e022f4960c
Jongmin Kim <kdzlvaids@gmail.com>
2015-07-01 22:12:08 +0900

    버튼 배열 변경, 단순화

--- TimeLap/frmMain.Designer.vb
+++ TimeLap/frmMain.Designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class frmMain
     Inherits System.Windows.Forms.Form
 
     'Form은 Dispose를 재정의하여 구성 요소 목록을 정리합니다.
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -20,11 +20,10 @@ Partial Class frmMain
     '참고: 다음 프로시저는 Windows Form 디자이너에 필요합니다.
     '수정하려면 Windows Form 디자이너를 사용하십시오.  
     '코드 편집기를 사용하여 수정하지 마십시오.
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
         Me.components = New System.ComponentModel.Container()
-        Me.btnCont = New System.Windows.Forms.Button()
-        Me.btnStop = New System.Windows.Forms.Button()
+        Me.btnReset = New System.Windows.Forms.Button()
         Me.btnStart = New System.Windows.Forms.Button()
         Me.txtRecord = New System.Windows.Forms.TextBox()
         Me.btnExit = New System.Windows.Forms.Button()
@@ -34,78 +33,71 @@ Partial Class frmMain
         Me.Timer = New System.Windows.Forms.Timer(Me.components)
         Me.SuspendLayout()
         '
-        'btnCont
+        'btnReset
         '
-        Me.btnCont.Location = New System.Drawing.Point(387, 119)
-        Me.btnCont.Name = "btnCont"
-        Me.btnCont.Size = New System.Drawing.Size(75, 23)
-        Me.btnCont.TabIndex = 15
-        Me.btnCont.Text = "계속"
-        Me.btnCont.UseVisualStyleBackColor = True
-        '
-        'btnStop
-        '
-        Me.btnStop.Location = New System.Drawing.Point(387, 71)
-        Me.btnStop.Name = "btnStop"
-        Me.btnStop.Size = New System.Drawing.Size(75, 23)
-        Me.btnStop.TabIndex = 14
-        Me.btnStop.Text = "정지"
-        Me.btnStop.UseVisualStyleBackColor = True
+        Me.btnReset.Location = New System.Drawing.Point(332, 129)
+        Me.btnReset.Name = "btnReset"
+        Me.btnReset.Size = New System.Drawing.Size(64, 25)
+        Me.btnReset.TabIndex = 1
+        Me.btnReset.Text = "다시 시작"
+        Me.btnReset.UseVisualStyleBackColor = True
         '
         'btnStart
         '
-        Me.btnStart.Location = New System.Drawing.Point(387, 26)
+        Me.btnStart.Location = New System.Drawing.Point(332, 28)
         Me.btnStart.Name = "btnStart"
-        Me.btnStart.Size = New System.Drawing.Size(75, 23)
-        Me.btnStart.TabIndex = 13
+        Me.btnStart.Size = New System.Drawing.Size(64, 25)
+        Me.btnStart.TabIndex = 0
         Me.btnStart.Text = "시작"
         Me.btnStart.UseVisualStyleBackColor = True
         '
         'txtRecord
         '
-        Me.txtRecord.Location = New System.Drawing.Point(25, 71)
+        Me.txtRecord.Location = New System.Drawing.Point(21, 70)
         Me.txtRecord.Multiline = True
         Me.txtRecord.Name = "txtRecord"
         Me.txtRecord.ReadOnly = True
         Me.txtRecord.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
-        Me.txtRecord.Size = New System.Drawing.Size(310, 166)
-        Me.txtRecord.TabIndex = 11
+        Me.txtRecord.Size = New System.Drawing.Size(266, 187)
+        Me.txtRecord.TabIndex = 6
+        Me.txtRecord.TabStop = False
         '
         'btnExit
         '
-        Me.btnExit.Location = New System.Drawing.Point(387, 214)
+        Me.btnExit.Location = New System.Drawing.Point(332, 231)
         Me.btnExit.Name = "btnExit"
-        Me.btnExit.Size = New System.Drawing.Size(75, 23)
-        Me.btnExit.TabIndex = 12
+        Me.btnExit.Size = New System.Drawing.Size(64, 25)
+        Me.btnExit.TabIndex = 3
         Me.btnExit.Text = "종료"
         Me.btnExit.UseVisualStyleBackColor = True
         '
         'btnRecord
         '
-        Me.btnRecord.Location = New System.Drawing.Point(387, 167)
+        Me.btnRecord.Location = New System.Drawing.Point(332, 180)
         Me.btnRecord.Name = "btnRecord"
-        Me.btnRecord.Size = New System.Drawing.Size(75, 23)
-        Me.btnRecord.TabIndex = 10
+        Me.btnRecord.Size = New System.Drawing.Size(64, 25)
+        Me.btnRecord.TabIndex = 2
         Me.btnRecord.Text = "기록"
         Me.btnRecord.UseVisualStyleBackColor = True
         '
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Font = New System.Drawing.Font("굴림", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(129, Byte))
-        Me.Label1.Location = New System.Drawing.Point(22, 30)
+        Me.Label1.Font = New System.Drawing.Font("Gulim", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(129, Byte))
+        Me.Label1.Location = New System.Drawing.Point(19, 32)
         Me.Label1.Name = "Label1"
         Me.Label1.Size = New System.Drawing.Size(53, 15)
-        Me.Label1.TabIndex = 9
+        Me.Label1.TabIndex = 4
         Me.Label1.Text = "Time :"
         '
         'txtTime
         '
-        Me.txtTime.Location = New System.Drawing.Point(81, 27)
+        Me.txtTime.Location = New System.Drawing.Point(78, 29)
         Me.txtTime.Name = "txtTime"
         Me.txtTime.ReadOnly = True
-        Me.txtTime.Size = New System.Drawing.Size(254, 21)
-        Me.txtTime.TabIndex = 8
+        Me.txtTime.Size = New System.Drawing.Size(209, 20)
+        Me.txtTime.TabIndex = 5
+        Me.txtTime.TabStop = False
         '
         'Timer
         '
@@ -113,11 +105,10 @@ Partial Class frmMain
         '
         'frmMain
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 12.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(484, 262)
-        Me.Controls.Add(Me.btnCont)
-        Me.Controls.Add(Me.btnStop)
+        Me.ClientSize = New System.Drawing.Size(415, 284)
+        Me.Controls.Add(Me.btnReset)
         Me.Controls.Add(Me.btnStart)
         Me.Controls.Add(Me.txtRecord)
         Me.Controls.Add(Me.btnExit)
@@ -131,8 +122,7 @@ Partial Class frmMain
         Me.PerformLayout()
 
     End Sub
-    Friend WithEvents btnCont As System.Windows.Forms.Button
-    Friend WithEvents btnStop As System.Windows.Forms.Button
+    Friend WithEvents btnReset As System.Windows.Forms.Button
     Friend WithEvents btnStart As System.Windows.Forms.Button
     Friend WithEvents txtRecord As System.Windows.Forms.TextBox
     Friend WithEvents btnExit As System.Windows.Forms.Button
@@ -140,5 +130,4 @@ Partial Class frmMain
     Friend WithEvents Label1 As System.Windows.Forms.Label
     Friend WithEvents txtTime As System.Windows.Forms.TextBox
     Friend WithEvents Timer As System.Windows.Forms.Timer
-
 End Class
---
+++ TimeLap/frmMain.vb
@@ -0,0 +1,43 @@
+﻿Public Class frmMain
+    Private Time As Double
+
+    Private Sub btnStart_Click(sender As Object, e As EventArgs) Handles btnStart.Click '
+        If Timer.Enabled = True Then    '타이머 정지
+            Timer.Enabled = False
+            btnStart.Text = "시작"
+            btnRecord.Enabled = False
+            btnReset.Enabled = True
+        Else                            '타이머 시작
+            Timer.Enabled = True
+            btnStart.Text = "정지"
+            btnRecord.Enabled = True
+            btnReset.Enabled = False
+        End If
+    End Sub
+
+    Private Sub btnReset_Click(sender As Object, e As EventArgs) Handles btnReset.Click
+        Time = 0.00
+        txtTime.Text = Format(Time, "f")
+        btnReset.Enabled = False
+        btnStart.Focus()
+    End Sub
+
+    Private Sub btnRecord_Click(sender As Object, e As EventArgs) Handles btnRecord.Click
+        txtRecord.AppendText(txtTime.Text + vbCrLf)
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        End
+    End Sub
+
+    Private Sub Timer_Tick(sender As Object, e As EventArgs) Handles Timer.Tick
+        Time += 0.01
+        txtTime.Text = Format(Time, "f")
+    End Sub
+
+    Private Sub frmMain_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Time = 0.00
+        txtTime.Text = Format(Time, "f")
+        btnReset.Enabled = False
+    End Sub
+End Class
